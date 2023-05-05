818546bebe641b30e04d9535e4c80ae47fde3b46
v.molotkov <v.molotkov@grohold.ru>
2023-05-05 09:25:20 +0300

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,58 @@
+"""
+URL configuration for app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from .settings import APP_NAME
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title=APP_NAME,
+        default_version='v1',
+        description=APP_NAME + ' description',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    patterns=[  # include your apps urls here for display in swagger
+        # path('', include('test_app.urls')),
+    ]
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', include('test_app.urls')),
+
+    # swagger urls
+    path(  # Swagger UI url
+        'docs/',
+        TemplateView.as_view(
+            template_name='swaggerui/swaggerui.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ),
+        name='swagger-ui'),
+    re_path(  # new
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
+]
